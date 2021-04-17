ad841af69d7afe26e2d23451d0e55d0efddd6d37
Wfmcodev <wfm.codevelopment@reply.it>
2021-04-17 23:41:03 +0200

    test2

--- test/routes/example.test.js
+++ test/routes/example.test.js
@@ -11,15 +11,15 @@ test('example is loaded', async (t) => {
   const res2 = await app.inject({
     url: '/example/select'
   })
-  // const res3 = await app.inject({
-  //   url: '/example/insertSQL'
-  // })
-  // const res4 = await app.inject({
-  //   url: '/example/selectSQL/0'
-  // })
+  const res3 = await app.inject({
+    url: '/example/insertSQL'
+  })
+  const res4 = await app.inject({
+    url: '/example/selectSQL/0'
+  })
   t.type(res1.payload, 'string')
   t.type(res2.payload, 'string')
-  // t.type(res3.payload, 'string')
-  // t.type(res4.payload, 'string')
+  t.type(res3.payload, 'string')
+  t.type(res4.payload, 'string')
 })
 
