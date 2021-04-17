0cb9d2e4c23a685a0ef0d2b89491747d69f71e25
Wfmcodev <wfm.codevelopment@reply.it>
2021-04-17 23:09:55 +0200

    test sql

--- test/routes/example.test.js
+++ test/routes/example.test.js
@@ -5,14 +5,21 @@ const { build } = require('../helper')
 
 test('example is loaded', async (t) => {
   const app = build(t)
-
   const res1 = await app.inject({
     url: '/example/insert'
   })
-  t.type(res1.payload, 'string')
   const res2 = await app.inject({
     url: '/example/select'
   })
+  const res3 = await app.inject({
+    url: '/example/insertSQL'
+  })
+  const res4 = await app.inject({
+    url: '/example/selectSQL/0'
+  })
+  t.type(res1.payload, 'string')
   t.type(res2.payload, 'string')
+  t.type(res3.payload, 'string')
+  t.type(res4.payload, 'string')
 })
 
